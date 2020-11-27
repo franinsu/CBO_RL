68d36a97a23546e2d2bc404764304fec3c3ecc26
Ubuntu <azureuser@RL.hn3tv4urlz2ujjrbxnsilr02yf.cwx.internal.cloudapp.net>
2020-11-27 10:34:07 +0000

    higher possible learning rate

--- cloud_script.py
+++ cloud_script.py
@@ -2,7 +2,7 @@
 import sys
 import pandas as pd
 _, problem_suffix, model_suffix= sys.argv[:3]
-resample, reQ, landscape, cuda, n_trials_sgd, n_trials_cbo, n_runs, N = [int(a) for a in sys.argv[3:]]
+resample, reQ, average, landscape,  cuda, n_trials_sgd, n_trials_cbo, n_runs, N = [int(a) for a in sys.argv[3:]]
 # %%
 import torch
 from torch.utils.tensorboard import SummaryWriter
@@ -96,7 +96,7 @@ def run_CBO_all(η_i,η_f,η_r,τ_i,τ_f,τ_r,β_i,β_f,β_r):
     return sum([np.log(e_s[-1]) for e_s in E])/len(E)
 # %%
 def objective(trial):
-    τ_i = trial.suggest_float("τ_i", 0., 1.)
+    τ_i = trial.suggest_float("τ_i", 0., 4.)
     τ_f = trial.suggest_float("τ_f", 0., 1.)
     τ_r = trial.suggest_float("τ_r", 0.95, 1.)
     return run_SGD_all(τ_i,τ_f,τ_r)
@@ -119,7 +119,7 @@ def objective(trial):
     η_i = trial.suggest_float("η_i", 0., 1.)
     η_f = trial.suggest_float("η_f", 0., 1.)
     η_r = trial.suggest_float("η_r", 0.9, 1.)
-    τ_i = trial.suggest_float("τ_i", 0., 1.)
+    τ_i = trial.suggest_float("τ_i", 0., 4.)
     τ_f = trial.suggest_float("τ_f", 0., 1.)
     τ_r = trial.suggest_float("τ_r", 0.9, 1.)
     β_i = trial.suggest_float("β_i", 5., 15.)
@@ -143,119 +143,120 @@ if n_trials_cbo>0:
 
 # %%
 print("\n\nAVERAGING RESULTS...\n")
-params = pickle.load(open(f"cache/sgd_params_{problem_suffix}_{model_suffix}.p", "rb"))
-τ_i, τ_f, τ_r = [params[x] for x in ['τ_i', 'τ_f', 'τ_f']]
-
-def τ_k(k):
-    return max(τ_i * τ_r ** k, τ_f*τ_i)
-
-sgd_args = {"τ_k": τ_k, "M": M}
-# %%
-def run_SGD_all():
-    global n_run
-    Qs, es = [], []
-    for s,u_s in sgd_u_s.items():
-        q, e = Q_SGD_gen(u_s)(
-            *args_0, **common_args, **sgd_args,main_tag=f"Averaging SGD {problem_suffix} {model_suffix}", scalar_tag=f"{s}_{n_run}")
-        Qs.append(q)
-        es.append(e)
-    return (Qs, es)
-
-# %%
-params = pickle.load(open(f"cache/cbo_params_{problem_suffix}_{model_suffix}.p", "rb"))
-η_i, η_f, η_r, τ_i, τ_f, τ_r, β_i, β_f, β_r = [params[x] for x in [
-    'η_i', 'η_f', 'η_r', 'τ_i', 'τ_f', 'τ_r', 'β_i', 'β_f', 'β_r']]
-early_stop = 1000
-def η_k(k): return max(η_i * η_r ** k, η_f*η_i)
-def τ_k(k): return max(τ_i * τ_r ** k, τ_f*τ_i)
-def β_k(k): return min(β_i * β_r ** k, β_f*β_i)
-
-
-cbo_args = {"N": N, "m": m, "τ_k": τ_k, "η_k": η_k,
-            "β_k": β_k, "δ": δ, "early_stop": early_stop}
-# %%
-def run_CBO_all():
-    global n_run
-    Qs, es = [], []
-    for s, u_s in cbo_u_s.items():
-        q, e = Q_CBO_gen(u_s)(
-            *args_0, **common_args, **cbo_args,main_tag=f"Averaging CBO {problem_suffix} {model_suffix}", scalar_tag=f"{s}_{n_run}")
-        Qs.append(q)
-        es.append(e)
-    return (Qs, es)
-# %%
-all_data = pd.DataFrame(columns=["i", "x", "y", "sampling", "algo", "plot"])
-y_star = Q_ctrl_UR_SGD_star(x_ls.view(-1, 1))
-a_n = len(a_s)
-for j, lb in enumerate(["UR", "DS", "BFF"]):
-    for k in range(a_n):
-        all_data = all_data.append(
-            pd.DataFrame(
-                {
-                    "i": 0,
-                    "x": x_ls.detach().numpy(),
-                    "y": y_star[:, k].detach().numpy(),
-                    "sampling": lb,
-                    "algo": "UR SGD *",
-                    "plot": f"$Q(a_{k},s)$",
-                }
+if average:
+    params = pickle.load(open(f"cache/sgd_params_{problem_suffix}_{model_suffix}.p", "rb"))
+    τ_i, τ_f, τ_r = [params[x] for x in ['τ_i', 'τ_f', 'τ_f']]
+    
+    def τ_k(k):
+        return max(τ_i * τ_r ** k, τ_f*τ_i)
+    
+    sgd_args = {"τ_k": τ_k, "M": M}
+    # %%
+    def run_SGD_all():
+        global n_run
+        Qs, es = [], []
+        for s,u_s in sgd_u_s.items():
+            q, e = Q_SGD_gen(u_s)(
+                *args_0, **common_args, **sgd_args,main_tag=f"Averaging SGD {problem_suffix} {model_suffix}", scalar_tag=f"{s}_{n_run}")
+            Qs.append(q)
+            es.append(e)
+        return (Qs, es)
+    
+    # %%
+    params = pickle.load(open(f"cache/cbo_params_{problem_suffix}_{model_suffix}.p", "rb"))
+    η_i, η_f, η_r, τ_i, τ_f, τ_r, β_i, β_f, β_r = [params[x] for x in [
+        'η_i', 'η_f', 'η_r', 'τ_i', 'τ_f', 'τ_r', 'β_i', 'β_f', 'β_r']]
+    early_stop = 1000
+    def η_k(k): return max(η_i * η_r ** k, η_f*η_i)
+    def τ_k(k): return max(τ_i * τ_r ** k, τ_f*τ_i)
+    def β_k(k): return min(β_i * β_r ** k, β_f*β_i)
+    
+    
+    cbo_args = {"N": N, "m": m, "τ_k": τ_k, "η_k": η_k,
+                "β_k": β_k, "δ": δ, "early_stop": early_stop}
+    # %%
+    def run_CBO_all():
+        global n_run
+        Qs, es = [], []
+        for s, u_s in cbo_u_s.items():
+            q, e = Q_CBO_gen(u_s)(
+                *args_0, **common_args, **cbo_args,main_tag=f"Averaging CBO {problem_suffix} {model_suffix}", scalar_tag=f"{s}_{n_run}")
+            Qs.append(q)
+            es.append(e)
+        return (Qs, es)
+    # %%
+    all_data = pd.DataFrame(columns=["i", "x", "y", "sampling", "algo", "plot"])
+    y_star = Q_ctrl_UR_SGD_star(x_ls.view(-1, 1))
+    a_n = len(a_s)
+    for j, lb in enumerate(["UR", "DS", "BFF"]):
+        for k in range(a_n):
+            all_data = all_data.append(
+                pd.DataFrame(
+                    {
+                        "i": 0,
+                        "x": x_ls.detach().numpy(),
+                        "y": y_star[:, k].detach().numpy(),
+                        "sampling": lb,
+                        "algo": "UR SGD *",
+                        "plot": f"$Q(a_{k},s)$",
+                    }
+                )
             )
-        )
-for n_run in range(n_runs):
-    for r, algo in [(run_SGD_all, "SGD"), (run_CBO_all, "CBO")]:
-        Q_s, e_s = r()
-        y_s = torch.stack([Q(x_ls.view(-1, 1)) for Q in Q_s])
-        y_s -= torch.unsqueeze(torch.mean(y_s - y_star, axis=1), 1)
-        for j, lb in enumerate(["UR", "DS", "BFF"]):
-            for k in range(a_n):
+    for n_run in range(n_runs):
+        for r, algo in [(run_SGD_all, "SGD"), (run_CBO_all, "CBO")]:
+            Q_s, e_s = r()
+            y_s = torch.stack([Q(x_ls.view(-1, 1)) for Q in Q_s])
+            y_s -= torch.unsqueeze(torch.mean(y_s - y_star, axis=1), 1)
+            for j, lb in enumerate(["UR", "DS", "BFF"]):
+                for k in range(a_n):
+                    all_data = all_data.append(
+                        pd.DataFrame(
+                            {
+                                "i": n_run,
+                                "x": x_ls.detach().numpy(),
+                                "y": y_s[j, :, k].detach().numpy(),
+                                "sampling": lb,
+                                "algo": algo,
+                                "plot": f"$Q(a_{k},s)$",
+                            }
+                        )
+                    )
                 all_data = all_data.append(
                     pd.DataFrame(
                         {
                             "i": n_run,
-                            "x": x_ls.detach().numpy(),
-                            "y": y_s[j, :, k].detach().numpy(),
+                            "x": np.arange(len(e_s[j])),
+                            "y": e_s[j] / e_s[j][0],
                             "sampling": lb,
                             "algo": algo,
-                            "plot": f"$Q(a_{k},s)$",
+                            "plot": r"$e_k/e_0$",
                         }
                     )
                 )
-            all_data = all_data.append(
-                pd.DataFrame(
-                    {
-                        "i": n_run,
-                        "x": np.arange(len(e_s[j])),
-                        "y": e_s[j] / e_s[j][0],
-                        "sampling": lb,
-                        "algo": algo,
-                        "plot": r"$e_k/e_0$",
-                    }
-                )
-            )
-
-# %%
-g = sns.FacetGrid(
-    data=all_data,
-    col="plot",
-    row="sampling",
-    hue="algo",
-    sharex=False,
-    sharey=False,
-    palette=["black", "C0", "C1"],
-    margin_titles=True,
-)
-g.map(sns.lineplot, "x", "y")
-g.set_axis_labels(x_var=f"$s$")
-for ax in g.axes[:, 2]:
-    ax.set_yscale("log")
-g.axes[-1, -1].set_xlabel(f"$k$")
-[plt.setp(ax.texts, text="") for ax in g.axes.flat]
-g.set_ylabels("")
-g.set_titles(col_template="{col_name}", row_template="{row_name}")
-plt.tight_layout()
-plt.subplots_adjust(right=0.9)
-g.add_legend(title="")
-plt.savefig(f"figs/Q_ctrl_SGD_vs_CBO_summary_{problem_suffix}_{model_suffix}.png")
+    
+    # %%
+    g = sns.FacetGrid(
+        data=all_data,
+        col="plot",
+        row="sampling",
+        hue="algo",
+        sharex=False,
+        sharey=False,
+        palette=["black", "C0", "C1"],
+        margin_titles=True,
+    )
+    g.map(sns.lineplot, "x", "y")
+    g.set_axis_labels(x_var=f"$s$")
+    for ax in g.axes[:, 2]:
+        ax.set_yscale("log")
+    g.axes[-1, -1].set_xlabel(f"$k$")
+    [plt.setp(ax.texts, text="") for ax in g.axes.flat]
+    g.set_ylabels("")
+    g.set_titles(col_template="{col_name}", row_template="{row_name}")
+    plt.tight_layout()
+    plt.subplots_adjust(right=0.9)
+    g.add_legend(title="")
+    plt.savefig(f"figs/Q_ctrl_SGD_vs_CBO_summary_{problem_suffix}_{model_suffix}.png")
 # %%
 if landscape:
     all_data = pd.DataFrame(columns=["i", "x", "y", "problem", "model"])
@@ -271,8 +272,8 @@ if landscape:
         for α in α_ls:
             with torch.no_grad():
                 for param, param_i, param_f in zip(Q_net.parameters(), Q_net_0.parameters(), Q_net_comp.parameters()):
-                    param = α * param_f + (1-α) * param_i
-            e.append(Q_comp(Q_net, Q_net_comp, x_ls, n).detach().numpy().item())
+                    param += α * param_f + (1-α) * param_i - param
+            e.append(Q_comp(Q_net, Q_net_comp, x_ls.view(-1,1), n).detach().numpy().item())
         all_data = all_data.append(
             pd.DataFrame(
                 {
@@ -284,8 +285,8 @@ if landscape:
                 }
             )
         )
-    fig = sns.lineplot(data=all_data, x="x", y="y", units="i",
+    ax = sns.lineplot(data=all_data, x="x", y="y", units="i",
                 alpha=0.7, hue="problem", estimator=None)
-    fig.write_image(f"figs/Q_ctrl_landscape_plot_{problem_suffix}_{model_suffix}.png", engine="kaleido")
+    ax.figure.savefig(f"figs/Q_ctrl_landscape_plot_{problem_suffix}_{model_suffix}.png")
 # %%
 writer.close()
---
+++ script.sh
@@ -0,0 +1,5 @@
+rm runs/* -rf
+# nohup ngrok http 6006 --log=stdout > ngrok.log &
+# nohup tensorboard --logdir=runs &
+nohup python3 ./cloud_script.py continuous resnet 0 0 1 0 0 100 100 10 90 > out1.log &
+nohup python3 ./cloud_script.py discrete tabular 0 0 1 0 0 100 100 10 90 > out2.log &
